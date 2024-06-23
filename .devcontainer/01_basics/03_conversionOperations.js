9329b2a27daa4394fbad8bb0c5acdcbc731317f4
rjoshi003 <joshirasikad@gmail.com>
2024-06-23 14:10:58 +0000

    conversions in js

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -24,5 +24,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIN);
 
 let someNumber = 33
 let stringSomeNumber = String(someNumber);
-console.log(stringSomeNumber);  //33
-console.log(typeof stringSomeNumber);  //string
+//console.log(stringSomeNumber);  //33
+//console.log(typeof stringSomeNumber);  //string
+
+//*********************OPERATIONS***************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue); //output-> -3 (negative of given value i.e. 3.)
+
+//console.log(2+2);-------->add
+//console.log(2-2);-------->sub
+//console.log(2*2);-------->multiply
+//console.log(2**3);------->power
+//console.log(2/3);-------->div
+//console.log(2%3);-------->modulo(remainder)
+
+
+let str1 = "hello"
+let str2 = "Rasika"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);    //agar string pehle h toh sabhi ko string ki tarah treat kiya jayeg
+//console.log(1 + 2 + "2");    //agar string baadme h toh pehle conversions ho jayenge then string execute hoga.
+
+//console.log(+true);
+//console.log(+ "");
+//prefix and posfix from mdn.
+//link to study-> https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
