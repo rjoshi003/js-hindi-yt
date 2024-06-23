598619e4069ac176bcb38bf74b7aee043bd7ba03
rjoshi003 <joshirasikad@gmail.com>
2024-06-23 12:55:26 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = undefined
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  
+
+//"33"-> 33
+//"33abc"-> NaN (type is still number!)
+// true-> 1; false-> 0;
+// undefined-> NaN
+
+
+let isLoggedIN = "Rasika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIN);
+//console.log(booleanIsLoggedIn);
+
+//1-> true; 0-> false;
+//""-> false
+//"Rasika"-> true
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);  //33
+console.log(typeof stringSomeNumber);  //string
