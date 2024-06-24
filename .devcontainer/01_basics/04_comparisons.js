98e6886a6ee742c53bb6012577be43d0ab86320a
rjoshi003 <joshirasikad@gmail.com>
2024-06-24 12:28:45 +0000

    comparisons

--- .devcontainer/01_basics/04_comparisons.js
+++ .devcontainer/01_basics/04_comparisons.js
@@ -4,4 +4,18 @@
 //console.log(2 == 1);
 //console.log(2 != 1);
 
-console.log("2" > 1);
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+console.log(null > 0);  //output-> false
+console.log(null == 0);   //output-> false
+console.log(null >= 0);   //output-> true
+
+console.log(undefined > 0);   //output-> false
+console.log(undefined < 0);   //output-> false
+console.log(undefined == 0);  //output-> false
+
+//  ===  (for strict check)
+
+console.log("2" === 2);  //output-> false
+console.log("2" == 2);  //output-> true
