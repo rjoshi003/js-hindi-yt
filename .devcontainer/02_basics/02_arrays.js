98071b4053d39a3a87329ddfa7c6acef3184b476
rjoshi003 <joshirasikad@gmail.com>
2024-07-01 08:23:29 +0000

    arrays

--- .devcontainer/02_basics/02_arrays.js
+++ .devcontainer/02_basics/02_arrays.js
@@ -10,4 +10,20 @@ marvel_heros.push(dc_heros) //array ke andar array. kisi bhi type ka inut le let
 // console.log(allHeros);
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
-console.log(all_new_heros);
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Hitiesh"));
+console.log(Array.from("Hitiesh"));
+console.log(Array.from({name: "Hitiesh"}));  //INTRESTING
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
