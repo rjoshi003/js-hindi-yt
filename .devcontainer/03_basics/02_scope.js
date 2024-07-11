c8fe381673dca90389778cee611b075116d4b203
rjoshi003 <joshirasikad@gmail.com>
2024-07-11 13:26:24 +0000

    scope

--- .devcontainer/03_basics/02_scope.js
+++ .devcontainer/03_basics/02_scope.js
@@ -9,19 +9,60 @@ let a = 300
 if (true){          //block scope
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 array = [1, 2, 3, 4,]
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
-console.log("ELEMENT: ", element);
+// console.log("ELEMENT: ", element);
 
 }
 //global scope.
 
 // console.log(a);
 // console.log(b);
-console.log(a);
-console.log(array);
+// console.log(a);
+// console.log(array);
 
 //code enviroment me and browser pe global scopes are diffrent.
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);  //scope ke bahar console nahi kar sakte!
+    //code line by line execute hota h, that's why two() execute hi nahi hua.
+    two()
+}
+// one()
+
+if (true){
+    const username = "hitesh"
+    if (username == "hitesh"){
+        const website = " youtube"
+        // console.log(username +  website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//+++++++++++++++++++++++ intresting +++++++++++++++++++++++++
+
+console.log(addone(5));
+
+
+function addone(num){
+    return num + 1
+}
+
+addTwo(5)
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
