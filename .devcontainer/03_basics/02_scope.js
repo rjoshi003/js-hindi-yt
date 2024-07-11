aa65fafcd6ef0f513160da3857a2f127920971e1
rjoshi003 <joshirasikad@gmail.com>
2024-07-11 12:59:23 +0000

    scope

--- .devcontainer/03_basics/02_scope.js
+++ .devcontainer/03_basics/02_scope.js
@@ -1,20 +1,27 @@
 
 
-//{}:-- scope
+//{}:-- scope   //var ki value scope ke bahar aajati hai.
+//{} with functions is scope. And without funtion or loop is an object.
+//console within scope.
 
 
 let a = 300
-if (true){
+if (true){          //block scope
     let a = 10
     const b = 20
     console.log("INNER: ", a);
 }
-
+array = [1, 2, 3, 4,]
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
-}
+console.log("ELEMENT: ", element);
 
+}
+//global scope.
 
 // console.log(a);
 // console.log(b);
 console.log(a);
+console.log(array);
+
+//code enviroment me and browser pe global scopes are diffrent.
